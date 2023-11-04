63d6ae715ea1b868868af4a9d7c80e0edae6915a
Yanik Kendler <70104756+elYanuki@users.noreply.github.com>
2023-11-05 00:41:37 +0100

    added existing table to frontend

--- frontend/web/public/scripts/index.js
+++ frontend/web/public/scripts/index.js
@@ -8,7 +8,7 @@ fetch('http://localhost:8080/equipment/create', {
     },
     body: JSON.stringify({ name: 'Lumix S5ii' })
 })
-    .then(function (response) { return response.json(); })
+    .then(function (response) { return response.text(); })
     .then(function (data) { return console.log(data); })
     .catch(function (error) { return console.error(error); });
 fetch("http://localhost:8080/equipment/getbyid/1/getname")
@@ -17,6 +17,31 @@ fetch("http://localhost:8080/equipment/getbyid/1/getname")
     return result.text();
 })
     .then(function (data) {
-    document.querySelector('p').innerText = data;
+    console.log(data);
 });
+var headings = [
+    "Gerät Nr.", "Zubehör", "Entlehner*in + Klasse", "Entlehnung Datum", "Unterschrift Entlehner*in", "Paraphe Lehkraft", "Rückgabe geplant",
+    "Rückgabe tatsächlich", "Unterschrift Entlehner*in", "Paraphe Lehkraft", "Anmerkung"
+];
+generateTable(20);
+function generateTable(rowCount) {
+    var headingHtml = document.createElement("tr");
+    var rowHtml = document.createElement("tr");
+    headings.forEach(function (heading) {
+        var headRow = document.createElement("th");
+        headRow.innerText = heading;
+        headingHtml.appendChild(headRow);
+        var emptyRow = document.createElement("td");
+        emptyRow.appendChild(document.createElement("input"));
+        rowHtml.appendChild(emptyRow);
+    });
+    var table = document.querySelector('table');
+    table.innerHTML = "";
+    table.appendChild(headingHtml);
+    var multiRowHtml = "";
+    for (var i = 0; i < rowCount; i++) {
+        multiRowHtml += "<tr>".concat(rowHtml.innerHTML, "</tr>");
+    }
+    table.innerHTML += multiRowHtml;
+}
 //# sourceMappingURL=index.js.map
--- frontend/web/public/scripts/index.ts
+++ frontend/web/public/scripts/index.ts
@@ -21,7 +21,43 @@ fetch('http://localhost:8080/equipment/create', {
          return result.text()
      })
      .then(data => {
-         document.querySelector('p').innerText = data
+         console.log(data)
      })
-
  //endregion
+
+
+const headings = [
+    "Gerät Nr.", "Zubehör", "Entlehner*in + Klasse", "Entlehnung Datum", "Unterschrift Entlehner*in", "Paraphe Lehkraft", "Rückgabe geplant",
+    "Rückgabe tatsächlich", "Unterschrift Entlehner*in", "Paraphe Lehkraft", "Anmerkung"
+]
+
+generateTable(20)
+function generateTable(rowCount: number){
+
+
+    let headingHtml = document.createElement("tr")
+    let rowHtml = document.createElement("tr")
+
+    //ATTENTION this approach only works as long as you dont keep this logic when updating to any kind of data storrage logic
+    //if it is kept this way, all rows would have the same content since they are made from the same row
+    headings.forEach(heading=>{
+        let headRow = document.createElement("th")
+        headRow.innerText = heading
+        headingHtml.appendChild(headRow)
+
+        let emptyRow = document.createElement("td")
+        emptyRow.appendChild(document.createElement("input"))
+        rowHtml.appendChild(emptyRow)
+    })
+
+    let table = document.querySelector('table')
+    table.innerHTML = ""
+
+    table.appendChild(headingHtml)
+
+    let multiRowHtml = ""
+    for (let i = 0; i < rowCount; i++) {
+        multiRowHtml += `<tr>${rowHtml.innerHTML}</tr>`
+    }
+    table.innerHTML += multiRowHtml
+}
