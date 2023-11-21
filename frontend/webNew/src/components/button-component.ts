2a295d70389a3d49c1f980d33a4b5adfc9a22305
Michael Leisch <michael.leisch@gmx.at>
2023-11-21 15:23:44 +0100

    pushing button to yanik

--- frontend/webNew/src/components/button-component.ts
+++ frontend/webNew/src/components/button-component.ts
@@ -1,16 +1,15 @@
 import {html, render} from "lit-html"
 
-import styles from '../../styles/components/button.scss'
-
-import styles from '../../styles/button.scss'
+import styles from "../../styles/components/button.scss"
 
 enum Size {MEDIUM="medium", SMALL="small"}
 enum Type {FILLED="filled", OUTLINED="outlined"}
 enum Color {ACCENT="accent", GRAY="gray"}
 
-const style = document.createElement("style")
 const button = (size: Size, type: Type, color: Color) => html`
-<link rel="stylesheet" href="/styles/components/button.css" >
+<!--
+<link rel="stylesheet" href="./target/styles/components/button.css" >
+-->
 <button class="cc-button" color="${color}" type="${type}" size="${size}">
     Button
 </button>`
@@ -20,6 +19,10 @@ class ButtonComponent extends HTMLElement {
     constructor() {
         super()
         const shadow = this.attachShadow({mode: "open"})
+
+/*        const styleTag = document.createElement('style');
+        styleTag.innerHTML = JSON.stringify(style).replace(/"/g, '');
+        shadow.appendChild(styleTag);*/
     }
     connectedCallback() {
         this.render()
