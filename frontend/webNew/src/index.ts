3811503911b9580609f673fe414a8f535739e9ce
Michael Leisch <michael.leisch@gmx.at>
2023-11-22 15:39:20 +0100

    rent status component created

--- frontend/webNew/src/index.ts
+++ frontend/webNew/src/index.ts
@@ -1,4 +1,5 @@
 import "./components/button-component"
+import "./components/rent-status-component"
 import "../styles/index.scss"
 
 //basically einfach default function file für erstellen clicken popups und alles
