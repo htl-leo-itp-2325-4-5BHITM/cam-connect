7b934d995887293cf1528126ca8e40632ab5e5f9
Yanik Kendler <70104756+elYanuki@users.noreply.github.com>
2023-11-25 00:37:43 +0100

    created cc error system

--- backend/src/main/java/at/camconnect/dtos/RentDTO.java
+++
@@ -1,6 +0,0 @@
-package at.camconnect.dtos;
-
-public class RentDTO {
-    public record rentComplete(long rent_id, long student_id, long device_id, long teacher_id, String rent_start, String rent_end_planned, String rent_end_actual, String note){
-    }
-}
