5075789c438b0ca68ae2819e974e5c8c5f89351a
Yanik Kendler <70104756+elYanuki@users.noreply.github.com>
2023-12-28 01:13:51 +0100

    Update select-component.ts

--- frontend/web/src/components/basic/select-component.ts
+++ frontend/web/src/components/basic/select-component.ts
@@ -1,22 +1,37 @@
 import {LitElement, html, PropertyValues} from 'lit'
-import {customElement, property} from 'lit/decorators.js'
+import {customElement, queryAssignedElements, property} from 'lit/decorators.js'
 import styles from '../../../styles/components/basic/select.styles.scss'
 
 @customElement('cc-select')
 export class SelectComponent extends LitElement {
-    render() {
-        const selectElements = Array.from(this.querySelectorAll('cc-select-element'))
+    @queryAssignedElements()
+    options!: Array<HTMLElement>
 
+    @property()
+    onSelect = () => {};
+
+    render() {
         return html`
             <style>${styles}</style>
             <div class="select">
                 <slot></slot>
             </div>`
     }
+
+    updated(changedProperties) {
+        this.options.forEach(option => {
+            option.addEventListener("click", ()=>{this.selectOption(option)})
+            console.log(option)
+        })
+    }
+
+    selectOption(option:HTMLElement){
+
+    }
 }
 
 declare global {
     interface HTMLElementTagNameMap {
-        "cc-select": SelectComponent;
+        "cc-select": SelectComponent
     }
 }
