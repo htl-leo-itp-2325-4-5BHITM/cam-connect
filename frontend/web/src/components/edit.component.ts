5061ee3aee1804efefc4c847aa480674def43a69
Yanik Kendler <70104756+elYanuki@users.noreply.github.com>
2024-04-19 20:30:45 +0200

    #93 refactoring

--- frontend/web/src/components/edit.component.ts
+++ frontend/web/src/components/edit.component.ts
@@ -14,14 +14,16 @@ export class EditComponent extends LitElement {
 
             <div>
                 <h2>Device Type Import</h2>
-                <select name="listOfTypes" id="listOfTypes" @change="${(event) => this.selectOption(event.target.value)}">
+                <select id="listOfTypes" @change="${(event) => this.selectOption(event.target.value)}">
                     ${listOfString.map(type => html`<option value="${type}">${type}</option>`)}
                 </select>
                 <div class="importBox">
                     <input type="file"
                            @change="${(event) => {
                                this.importDataFromCsv(event, "devicetype", listOfString[0])
-                           }}" accept=".csv"/>
+                           }}" 
+                           accept=".csv"
+                    />
                 </div>
             </div>
 
@@ -30,23 +32,32 @@ export class EditComponent extends LitElement {
                 <input type="file"
                        @change="${(event) => {
                            this.importDataFromCsv(event, "device", "")
-                       }}" accept=".csv"/>
+                       }}" 
+                       accept=".csv"
+                />
             </div>
-        `;
-    }
-    selectOption(type) {
-        const importBox = this.shadowRoot.querySelector('.importBox');
-        importBox.innerHTML = '';
-        const input = document.createElement('input');
-        input.type = 'file';
-        input.accept = '.csv';
-        input.addEventListener('change', (event) => {
-            this.importDataFromCsv(event, "devicetype", type)
-        });
-        importBox.appendChild(input);
+        `
     }
 
-    importDataFromCsv(event: Event, importType: string, type:string, ) {
+    selectOption(type) {
+        //TODO why is this code structured like this
+        //you completely remove and re add the input instead of simply changing the type
+        //either change this to a class variable that or use a data-type property on the input
+        //both can be easily referenced in the @click binding on the input
+
+        const importBox = this.shadowRoot.querySelector('.importBox')
+        importBox.innerHTML = ''
+
+        const input = document.createElement('input')
+        input.type = 'file'
+        input.accept = '.csv'
+        input.addEventListener('change', (event) => {
+            this.importDataFromCsv(event, "devicetype", type)
+        })
+        importBox.appendChild(input)
+    }
+
+    importDataFromCsv(event: Event, importType: string, type:string) {
         let input = event.target as HTMLInputElement
         let file:File = input.files[0]
         const formData = new FormData()
@@ -54,22 +65,17 @@ export class EditComponent extends LitElement {
 
         Api.postData(`/${importType}/import/${type}`, formData, "upload")
             .then((data) => {
-                console.log(data)
                 switch (data.ccStatus.statusCode){
                     case 1000:
-                        //@ts-ignore
                         PopupEngine.createNotification({text: `Successfully imported ${type}`, CSSClass: "good"})
                         break
                     case 1201:
-                        //@ts-ignore
                         PopupEngine.createNotification({text: `Konnte nicht importieren, weil der DeviceType bereits existiert`, CSSClass: "bad"})
                         break
                     case 1203:
-                        //@ts-ignore
                         PopupEngine.createNotification({text: `Konnte nicht importiert werden, weil das File leer ist`, CSSClass: "bad"})
                         break
                     case 1204:
-                        //@ts-ignore
                         PopupEngine.createNotification({text: `Konnte nicht importieren, weil die filestruktur invalide ist`, CSSClass: "bad"})
                         break
                 }})
